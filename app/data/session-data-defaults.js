e753d22e214203cc1f61f391feb36a9aa572142c
Chris Hanson <chrisphanson@outlook.com>
2018-06-11 11:55:48 +0100

    Using the default session storage to setup claimants, rather than JSON file, as we don't want to store any details entered during user research.

--- app/data/session-data-defaults.js
+++ app/data/session-data-defaults.js
@@ -19,6 +19,82 @@ Example usage:
 
 module.exports = {
 
-  // Insert values here
+  "claimants": [
+    {
+      "nino": "QQ123456C",
+      "firstName": "Joe",
+      "lastName": "Bloggs",
+      "dob": [
+        "1980",
+        "1",
+        "1"
+      ],
+      "address": {
+        "street": "23 River View",
+        "street2": "",
+        "city": "Bedlington",
+        "postcode": "NE22 5LS"
+      },
+      "partner": [
+        "true"
+      ],
+      "partnerName": "Jane Doe",
+      "partnerCode": "1234",
+      "appointee": [
+        "true"
+      ],
+      "appointeeName": "Richard Roe",
+      "legacyBenefits": [
+        "ctc",
+        "hb"
+      ],
+      "welsh": [
+        "true"
+      ],
+      "landline": "",
+      "mobile": "07000000000",
+      "history": [
+        {
+          "title": "Orientation letter sent",
+          "body": "Re-issue letter",
+          "link": "#0",
+          "time": "Automated, 1 Jan 2018, 14:11"
+        },
+        {
+          "title": "Claimant call",
+          "body": "Claimant called to confirm that they didn't need to apply yet. Captured mobile number while on call.",
+          "time": "Rachael Stewart, 9 Jan 2018, 10:10"
+        }
+      ]
+    },
+    {
+      "nino": "UU123456C",
+      "firstName": "Jane",
+      "lastName": "Doe",
+      "dob": [
+        "1972",
+        "12",
+        "12"
+      ],
+      "address": {
+        "street": "23 River View",
+        "street2": "",
+        "city": "Bedlington",
+        "postcode": "NE22 5LS"
+      },
+      "partner": [
+        "true"
+      ],
+      "partnerName": "Joe Bloggs",
+      "partnerCode": "1234",
+      "appointee": "_unchecked",
+      "appointeeName": "",
+      "legacyBenefits": [
+        "ctc"
+      ],
+      "welsh": "_unchecked"
+    }
+  ]
+
 
 }
--- app/routes.js
+++ app/routes.js
@@ -1,8 +1,5 @@
 const express = require('express')
 const router = express.Router()
-const fs = require('fs')
-// Claimant data
-const data = require('./assets/claimants.json')
 
 // Route index page
 router.get('/', function (req, res) {
@@ -11,25 +8,15 @@ router.get('/', function (req, res) {
 
 // On sign out delete session data
 router.get('/signout', function (req, res) {
-  // var starter = require('./assets/claimants-starter.json')
-  for (var i = 1; i < data.length; i++) {
-    delete data[i];
-  }
-  fs.writeFile('app/assets/claimants.json', JSON.stringify(data,null,2), (err) => {
-    if (err) throw err;
-    console.log('JSON reset');
-  })
-
-
   req.session.destroy()
   res.redirect('/')
 })
 
-// Search claimants from JSON file and render claimant view or no results view
+// Search claimants from default session data file and render claimant view or no results view
 router.get('/results', (req, res) => {
-  res.locals.claimants = data;
   const search = req.session.data['find'].replace(/ /g,'').toUpperCase()
-  const claimant = data.filter(claimant => claimant.nino === search)
+  const claimant = req.session.data['claimants'].filter(claimant => claimant.nino === search)
+
   res.locals.claimant = claimant[0]
 
   if (claimant == false) {
@@ -39,55 +26,16 @@ router.get('/results', (req, res) => {
   }
 })
 
-// Create a new claimant
-router.post('/create', (req, res) => {
-  const newItem = Object.assign({
-    nino: req.body.nino.replace(/ /g,'').toUpperCase(),
-    firstName: req.body.firstName,
-    lastName: req.body.lastName,
-    dob: [
-      req.body.dobYear,
-      req.body.dobMonth,
-      req.body.dobDay
-    ],
-    // phone: req.body.phone,
-    address: {
-      street: req.body.addressStreet1,
-      street2: req.body.addressStreet2,
-      city: req.body.addressCity,
-      postcode: req.body.addressPostcode
-    },
-    landline: req.body.landline,
-    mobile: req.body.mobile,
-    partner: req.body.partner,
-    partnerName: req.body.partnerName,
-    partnerCode: req.body.partnerCode,
-    appointee: req.body.appointee,
-    appointeeName: req.body.appointeeName,
-    legacyBenefits: req.body.legacyBenefits,
-    welsh: req.body.welsh
-  })
-  data.push(newItem)
-  fs.writeFile('app/assets/claimants.json', JSON.stringify(data,null,2), (err) => {
-    if (err) throw err;
-    console.log('Claimant created');
-  })
-  res.redirect('/results?find='+req.body.nino)
-  delete req.session.data['find']
-})
-
 // Insert claimant details into edit view
 router.get('/claimant/edit/:nino', (req, res) => {
-  res.locals.claimants = data;
-  const claimantToEdit = data.filter(claimant => claimant.nino === req.params.nino)
+  const claimantToEdit = req.session.data['claimants'].filter(claimant => claimant.nino === req.params.nino)
   res.locals.claimant = claimantToEdit[0]
   res.render('claimant/edit')
 })
 
 // Update claimant record
 router.post('/claimant/edit/:nino', (req, res) => {
-  res.locals.claimants = data;
-  let claimantToEdit = data.filter(claimant => claimant.nino === req.params.nino)
+  let claimantToEdit = req.session.data['claimants'].filter(claimant => claimant.nino === req.params.nino)
   claimantToEdit[0].nino = req.body.nino.replace(/ /g,'').toUpperCase(),
   claimantToEdit[0].firstName = req.body.firstName,
   claimantToEdit[0].lastName = req.body.lastName,
@@ -113,12 +61,12 @@ router.post('/claimant/edit/:nino', (req, res) => {
   claimantToEdit[0].legacyBenefits = req.body.legacyBenefits,
   claimantToEdit[0].welsh = req.body.welsh
 
-  fs.writeFile('app/assets/claimants.json', JSON.stringify(data, null, 2), (err) => {
-    if (err) throw err;
-    console.log('Claimant updated');
-  });
   res.redirect('/results?find='+req.body.nino);
 })
+
+
+
+
 
 
 // Add your routes above the module.exports line
