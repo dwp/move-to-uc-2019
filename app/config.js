8d46a5d6c91537a6bb29fcf1663bbe5c7c3c7753
Sam <sameastwood.digitaldwp@gmail.com>
2019-05-24 16:01:38 +0100

    UR amends

--- app/config.js
+++ app/config.js
@@ -4,7 +4,7 @@
 
 module.exports = {
   // Service name used in header. Eg: 'Renew your passport'
-  serviceName: 'Track a move to UC',
+  serviceName: 'Manage a move to UC',
 
   // Default port that prototype runs on
   port: '3000',
