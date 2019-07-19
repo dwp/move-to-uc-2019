f796044341865a2ed52ef3430cf6e2a578d3dfee
Sam <sameastwood.digitaldwp@gmail.com>
2019-07-19 10:37:17 +0100

    exclude and defer amends

--- app/routes.js
+++ app/routes.js
@@ -316,16 +316,44 @@ router.post('/nomoreaction/confirm-answer', function (req, res) {
   }
 })
 
+// router.post('/cancel/confirm-answer', function (req, res) {
+//
+//   let cancel = req.session.data['cancel']
+//
+//   if (cancel === 'died') {
+//     res.redirect('success-died')
+//   // } else if (migrationnotice === 'no') {
+//   //   res.redirect('no')
+//   } else {
+//     res.redirect('success')
+//   }
+// })
+
+
 router.post('/cancel/confirm-answer', function (req, res) {
 
-  let cancel = req.session.data['cancel']
+ let cancel = req.session.data['cancel']
 
-  if (cancel === 'died') {
-    res.redirect('success-died')
+ if (cancel === 'died') {
+   res.redirect('../cancel/success-died')
+ } else if (cancel === 'exclude') {
+    res.redirect('../cancel/success-exclude')
+ } else {
+   res.redirect('../cancel/success')
+ }
+})
+
+
+router.post('/pauseexclude/confirm-answer', function (req, res) {
+
+  let pauseexclude = req.session.data['pauseexclude']
+
+  if (pauseexclude === 'defer') {
+    res.redirect('success-defer')
   // } else if (migrationnotice === 'no') {
   //   res.redirect('no')
   } else {
-    res.redirect('success')
+    res.redirect('success-exclude')
   }
 })
 
